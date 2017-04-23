5c38e3fcccb1baf5be273b5c81c4550d06559032
Noah Klein <noahklein94@gmail.com>
2017-04-22 21:54:00 -0400

    index files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TextPlayer from './TextPlayer';
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+
+function App() {
+  return <TextPlayer />;
+}
